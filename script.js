5ea6610f105d0d3c61d7b527cd4f501128520d5b
Jasmine Greenaway <paladique@users.noreply.github.com>
2025-10-03 16:05:32 +0000

    inital notes

--- script.js
+++ script.js
@@ -1,1 +1,44 @@
+console.log("Hello!")
+console.log(1+1)
 
+//alert("Hello!")
+//alert(1 + 1)
+
+let myfirstvar
+myfirstvar = 100
+
+let mysecondvar = "Hello!"
+
+//alert(myfirstvar)
+console.log(myfirstvar)
+
+//alert(mysecondvar)
+console.log(mysecondvar)
+
+let undefinedvar
+console.log(undefinedvar)
+
+let comparison1 = 100 == 100
+console.log(comparison1)
+
+console.log(100 > 100)
+console.log(100 >= 100)
+
+let num1 = 10
+let num2 = 50
+
+console.log(num1 > num2)
+console.log(num1 < num2)
+
+console.log(num1 * 5 >= num2)
+
+console.log(num1 * 5)
+
+
+if (num1 < num2){
+    console.log("This is true")
+}
+
+else {
+    console.log("This is false!")
+}
